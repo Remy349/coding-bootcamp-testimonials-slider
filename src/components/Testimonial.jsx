92ce625467b2e192bde20bfcd6739cfee0dae93c
Remy349 <santoraga15@gmail.com>
2023-02-25 15:32:03 -0600

    Project completed

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -15,17 +15,23 @@ const TestimonialButton = ({ img, handleClick }) => {
 const Testimonial = ({ img, testimonial, name, job }) => {
   const next = () => {
     const slider = document.querySelector('.slider')
-    slider.style.transform = 'translateX(-50%)'
+    slider.style.marginLeft = '-100%'
   }
 
   const prev = () => {
     const slider = document.querySelector('.slider')
-    slider.style.transform = 'translateX(0%)'
+    slider.style.marginLeft = '0%'
   }
 
   return (
-    <figure className='slider__section flex items-center flex-col gap-y-16'>
-      <div className='slider__section-image flex flex-col items-center w-64 relative'>
+    <figure
+      className='slider__section flex items-center flex-col gap-y-16
+      lg:flex-row lg:items-center'
+    >
+      <div
+        className='slider__section-image flex flex-col items-center w-64 relative
+        lg:order-1 lg:w-3/5 xl:ml-20'
+      >
         <img
           className='slider__section-img w-64 h-full rounded-lg shadow-md'
           src={img}
@@ -33,17 +39,24 @@ const Testimonial = ({ img, testimonial, name, job }) => {
         />
         <div
           className='flex bg-white shadow-md absolute -bottom-5 rounded-t-full
-          rounded-b-full overflow-hidden'
+          rounded-b-full overflow-hidden lg:left-36'
         >
           <TestimonialButton img={iconPrev} handleClick={prev} />
           <TestimonialButton img={iconNext} handleClick={next} />
         </div>
       </div>
-      <div className='slider__section-testimonial text-center'>
+      <div
+        className='slider__section-testimonial text-center lg:text-left
+        lg:w-2/5'
+      >
         <blockquote className='mb-4'>
-          <p className='text-dark-blue font-light text-lg'>{testimonial}</p>
+          <p
+            className='text-dark-blue font-light text-lg lg:text-2xl xl:text-4xl'
+          >
+            {testimonial}
+          </p>
         </blockquote>
-        <figcaption>
+        <figcaption className='lg:flex lg:items-center lg:gap-x-2 lg:text-lg xl:text-xl'>
           <p className='text-dark-blue font-bold'>{name}</p>
           <span className='text-grayish-blue font-medium'>{job}</span>
         </figcaption>
